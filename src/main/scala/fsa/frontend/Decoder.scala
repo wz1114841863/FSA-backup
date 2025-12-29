c974d29543e42b4135103e4030309f1493f3f255
wz1114841863 <18801383735@163.com>
2025-12-29 19:32:33 +0800

    add anno in fronted

--- src/main/scala/fsa/frontend/Decoder.scala
+++ src/main/scala/fsa/frontend/Decoder.scala
@@ -9,61 +9,77 @@ import fsa.isa.ISA.Constants.I_TYPE_BITS
 import org.chipsalliance.cde.config.Parameters
 
 class InstructionMerger(n: Int) extends Module {
-  val io = IO(new Bundle {
-    val in = Flipped(Decoupled(UInt(32.W)))
-    val out = Decoupled(UInt((n * 32).W))
-    val inflight = Output(Bool())
-  })
-  val buf = Reg(Vec(n, UInt(32.W)))
-  val cnt = RegInit(0.U(n.U.getWidth.W))
+    // 用于将多个32位指令字合并成一个宽指令
+    val io = IO(new Bundle {
+        val in = Flipped(Decoupled(UInt(32.W)))
+        val out = Decoupled(UInt((n * 32).W))
+        val inflight = Output(Bool())
+    })
+    val buf = Reg(Vec(n, UInt(32.W))) // n个32位寄存器组成的缓冲区
+    val cnt = RegInit(0.U(n.U.getWidth.W)) // 计数器,记录已接收的指令数量
 
-  val w_addr = Mux(io.out.fire, 0.U, cnt)
+    val w_addr = Mux(io.out.fire, 0.U, cnt) // 写地址
 
-  when(io.in.fire) {
-    buf(w_addr) := io.in.bits
-    cnt := cnt + 1.U
-  }
-  when(io.out.fire) {
-    cnt := io.in.fire.asUInt
-  }
-  io.out.valid := cnt === n.U
-  io.out.bits := buf.asUInt
-  io.in.ready := cnt < n.U || cnt === n.U && io.out.fire
-  io.inflight := cnt =/= 0.U && !io.out.valid
+    when(io.in.fire) {
+        buf(w_addr) := io.in.bits
+        cnt := cnt + 1.U
+    }
+    when(io.out.fire) {
+        cnt := io.in.fire.asUInt
+    }
+    io.out.valid := cnt === n.U // 收齐n个字后输出有效
+    io.out.bits := buf.asUInt
+    io.in.ready := cnt < n.U || cnt === n.U && io.out.fire // 缓冲区有空位或能及时输出时可接收新字
+    io.inflight := cnt =/= 0.U && !io.out.valid // 有部分数据但未收齐
 }
 
 class Decoder(memAddrWidth: Int)(implicit p: Parameters) extends FSAModule {
-  val io = IO(new Bundle {
-    val in = Flipped(Decoupled(UInt(32.W)))
-    val outMx = Decoupled(new MatrixInstruction(SPAD_ROW_ADDR_WIDTH, ACC_ROW_ADDR_WIDTH))
-    val outDMA = Decoupled(new DMAInstruction(SRAM_ROW_ADDR_WIDTH, memAddrWidth))
-    val outFence = Decoupled(new FenceInstruction)
-  })
-  val mx = Module(new InstructionMerger(3))
-  val dma = Module(new InstructionMerger(4))
-  assert(!(mx.io.inflight && dma.io.inflight))
+    // 指令解码器
+    val io = IO(new Bundle {
+        val in = Flipped(Decoupled(UInt(32.W)))
+        val outMx = Decoupled(
+          new MatrixInstruction(SPAD_ROW_ADDR_WIDTH, ACC_ROW_ADDR_WIDTH)
+        )
+        val outDMA =
+            Decoupled(new DMAInstruction(SRAM_ROW_ADDR_WIDTH, memAddrWidth))
+        val outFence = Decoupled(new FenceInstruction)
+    })
+    val mx = Module(new InstructionMerger(3))
+    val dma = Module(new InstructionMerger(4))
+    // 确保不会同时处理两种多字指令
+    // 因为first标志确保同一时间只有一个合并器在工作
+    assert(!(mx.io.inflight && dma.io.inflight))
 
-  val instType = io.in.bits.head(I_TYPE_BITS)
-  val first = !mx.io.inflight && !dma.io.inflight
-  val selMx = first && instType === InstTypes.MATRIX.U || mx.io.inflight
-  val selDma = first && instType === InstTypes.DMA.U || dma.io.inflight
-  val selFence = first && instType === InstTypes.FENCE.U
+    val instType = io.in.bits.head(I_TYPE_BITS) // 低三位, 指令类型字段
+    val first = !mx.io.inflight && !dma.io.inflight // 是否是当前指令的第一个字
+    // 根据instType和当前状态选择处理路径
+    val selMx = first && instType === InstTypes.MATRIX.U || mx.io.inflight
+    val selDma = first && instType === InstTypes.DMA.U || dma.io.inflight
+    val selFence = first && instType === InstTypes.FENCE.U
+    // 输入连接
+    mx.io.in.valid := selMx && io.in.valid
+    mx.io.in.bits := io.in.bits
+    dma.io.in.valid := selDma && io.in.valid
+    dma.io.in.bits := io.in.bits
+    // MX输出连接
+    io.outMx.valid := mx.io.out.valid
+    io.outMx.bits := mx.io.out.bits.asTypeOf(io.outMx.bits)
+    mx.io.out.ready := io.outMx.ready
+    // DMA输出连接
+    io.outDMA.valid := dma.io.out.valid
+    io.outDMA.bits := dma.io.out.bits.asTypeOf(io.outDMA.bits)
+    dma.io.out.ready := io.outDMA.ready
+    // 单字指令,直接解码输出
+    io.outFence.valid := selFence && io.in.valid
+    io.outFence.bits := io.in.bits.asTypeOf(io.outFence.bits)
 
-  mx.io.in.valid := selMx && io.in.valid
-  mx.io.in.bits := io.in.bits
-  dma.io.in.valid := selDma && io.in.valid
-  dma.io.in.bits := io.in.bits
-
-  io.outMx.valid := mx.io.out.valid
-  io.outMx.bits := mx.io.out.bits.asTypeOf(io.outMx.bits)
-  mx.io.out.ready := io.outMx.ready
-
-  io.outDMA.valid := dma.io.out.valid
-  io.outDMA.bits := dma.io.out.bits.asTypeOf(io.outDMA.bits)
-  dma.io.out.ready := io.outDMA.ready
-
-  io.outFence.valid := selFence && io.in.valid
-  io.outFence.bits := io.in.bits.asTypeOf(io.outFence.bits)
-
-  io.in.ready := Mux(selMx, mx.io.in.ready, Mux(selDma, dma.io.in.ready, io.outFence.ready))
+    io.in.ready := Mux(
+      selMx,
+      mx.io.in.ready, // Matrix指令时看mx合并器是否就绪
+      Mux(
+        selDma,
+        dma.io.in.ready, // DMA指令时看dma合并器是否就绪
+        io.outFence.ready // Fence指令时看输出端口是否就绪
+      )
+    )
 }
--- src/main/scala/fsa/frontend/Semaphores.scala
+++ src/main/scala/fsa/frontend/Semaphores.scala
@@ -4,33 +4,49 @@ import chisel3._
 import chisel3.util._
 import fsa.isa.ISA.Constants._
 
+/*
+信号量控制流程:
+    发射前:检查acquireValid,等待信号量就绪
+    发射时:标记信号量为忙(防止其他指令获取)
+    执行中:保持忙状态
+    完成后:根据releaseValid更新信号量值并清除忙状态
+ */
 class Semaphore extends Bundle {
-  val id = UInt(SEM_ID_BITS.W)
-  val value = UInt(SEM_VALUE_BITS.W)
+    val id = UInt(SEM_ID_BITS.W) // 5位, 支持32个信号量
+    val value = UInt(SEM_VALUE_BITS.W) // 3位, 值范围0-7
 }
 
-
 class Semaphores(nRead: Int, nWrite: Int) extends Module {
-  val io = IO(new Bundle {
-    val acquire = Vec(nRead, Flipped(Decoupled(new Semaphore)))
-    val release = Vec(nWrite, Flipped(Valid(new Semaphore)))
-  })
+    val io = IO(new Bundle {
+        val acquire =
+            Vec(nRead, Flipped(Decoupled(new Semaphore))) // 获取请求(握手接口)
+        val release = Vec(nWrite, Flipped(Valid(new Semaphore))) // 释放请求(有效接口)
+    })
 
-  val semaphores = RegInit(VecInit(Seq.fill(N_SEMAPHORES){0.U(SEM_VALUE_BITS.W)}))
-  val busy = RegInit(VecInit(Seq.fill(N_SEMAPHORES){ false.B }))
+    val semaphores = RegInit(VecInit(Seq.fill(N_SEMAPHORES) {
+        0.U(SEM_VALUE_BITS.W) // 32个信号量,每个3位,初始值为0
+    }))
+    // 每个信号量一个忙标志,防止重复获取
+    val busy = RegInit(VecInit(Seq.fill(N_SEMAPHORES) { false.B }))
 
-  io.release.foreach{ release =>
-    when(release.fire) {
-      busy(release.bits.id) := false.B
-      semaphores(release.bits.id) := release.bits.value
+    io.release.foreach { release =>
+        when(release.fire) { // release.fire = release.valid(Valid接口的特性)
+            busy(release.bits.id) := false.B // 清除忙状态
+            semaphores(release.bits.id) := release.bits.value // 更新信号量值
+        }
     }
-  }
 
-  io.acquire.foreach{ acq =>
-    acq.ready := !busy(acq.bits.id) && acq.bits.value === semaphores(acq.bits.id)
-    when(acq.fire) {
-      busy(acq.bits.id) := true.B
+    // 两个条件必须同时满足:
+    // 1. 信号量不忙(!busy(id))
+    // 2. 请求值等于当前值(value === semaphores(id))
+    io.acquire.foreach { acq =>
+        acq.ready := !busy(acq.bits.id) && acq.bits.value === semaphores(
+          acq.bits.id
+        )
+        when(acq.fire) {
+            // 标记为忙,防止其他请求获取
+            busy(acq.bits.id) := true.B
+        }
     }
-  }
 
 }
